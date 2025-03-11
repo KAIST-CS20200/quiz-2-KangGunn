fcbf230171982ce5804245a6962e6371a92d24eb
yhtlaeh <102238524+KangGunn@users.noreply.github.com>
2025-03-11 10:04:28 +0900

    Cha

--- Math.fs
+++ Math.fs
@@ -6,4 +6,9 @@ open System
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
-  0
+  let n = int64 n
+  let rec iter product count =
+    if product = 1L then count
+    elif product % 2L = 0L then iter (product/2L) (count+1)
+    else iter (3L*product + 1L) (count+1)
+  iter n 0
